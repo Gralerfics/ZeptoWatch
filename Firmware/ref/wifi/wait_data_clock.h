1f6b4ac66026d4537a0909d9829c7d5087b80e63
R L <glverfer@outlook.com>
2022-11-20 22:58:41 +0800

    style(gitignore): some unimportant work.

--- Firmware/ref/wifi/wait_data_clock.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _WAIT_DATA_CLOCK_H
-#define _WAIT_DATA_CLOCK_H
-
-
-void wifi_wait_data_hander(void);
-
-
-#endif
--- Firmware/ref/wifi/wifi_cmd_task.h
+++
@@ -1,8 +0,0 @@
-#ifndef _WIFI_CMD_TASK_H_
-#define _WIFI_CMD_TASK_H_
-
-#include "common.h"
-
-void wifi_comd_open(void);
-void WiFi_CMD_updata(void);
-#endif
