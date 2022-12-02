02e6526fa7593c7a13c8f228934a34f7fb799956
R L <glverfer@outlook.com>
2022-12-03 00:54:16 +0800

    style(ui).

---
+++ Firmware/ZeptoWatch/UI/ui.h
@@ -0,0 +1,31 @@
+// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.1.1
+// LVGL VERSION: 8.2.0
+// PROJECT: est
+
+#ifndef _EST_UI_H
+#define _EST_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvgl/lvgl.h"
+
+extern lv_obj_t * ui_Screen1;
+extern lv_obj_t * ui_Screen1_Keyboard1;
+extern lv_obj_t * ui_Screen1_TextArea1;
+extern lv_obj_t * ui_Screen1_Label1;
+
+
+
+
+
+
+void ui_init(void);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
